3decb9f5c17b7926b39c90f3d48b8b29f666a466
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-07-11 21:07:23 +0530

    VN_A2_01: input component added

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent {
-  
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() text = '';
+  @Input() inputId = '';
+  @Input() value = '';
+  @Input() type: 'text' | 'email' | 'password' = 'text';
+  @Input() required = false;
+  @Output() valueChange = new EventEmitter<string>();
+
+  isFocused = false;
+  isTouched = false;
+
+  get showRequiredError(): boolean {
+      return this.required && this.isTouched && !this.isFocused && !this.value.trim();
+  }
+
+  onFocus(): void {
+      this.isFocused = true;
+  }
+
+  onBlur(): void {
+      this.isFocused = false;
+      this.isTouched = true;
+  }
+
+  onInput(event: Event): void{
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;
+    this.valueChange.emit(this.value);
+  }
 }
