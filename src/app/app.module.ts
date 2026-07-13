836a971ccbdfe3f40f428f4b25a84f73bb7adaa3
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-07-13 23:39:05 +0530

    VN_A2_01: all material ui components imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,6 +25,11 @@ import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.c
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule
   ],
 >>>>>>> de30a38 (VN_A2_01: button component added)
   providers: [],
