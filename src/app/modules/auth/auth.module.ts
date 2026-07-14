bd5bf3c3d929395c9d56d85bde79e3f33ee86e74
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-07-14 16:28:01 +0530

    VN_A2_02: login component ui added

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 import { AuthRoutingModule } from './auth-routing-module';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
 <<<<<<< HEAD
@@ -15,7 +17,9 @@ import { LoginComponent } from './login/login.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatCardModule,
+    SharedModule
   ]
 >>>>>>> c3785f0 (VN_A2_02: set up auth routing along with the user roles for all pages on websites)
 })
