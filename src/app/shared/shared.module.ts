df513754def9b13b300ac945f4e682ef58e1f7c6
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-07-15 11:52:12 +0530

    VN_A2_03: header along with its layout and styling added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 import { ButtonComponent } from '@shared/components/button/button.component';
 import { CardComponent } from '@shared/components/card/card.component';
 import { ChipInputComponent } from '@shared/components/chip-input/chip-input.component';
 import { DropdownComponent } from '@shared/components/dropdown/dropdown.component';
 import { InputComponent } from '@shared/components/input/input.component';
 import { OrderCardComponent } from '@shared/components/order-card/order-card.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { HeaderComponent } from '@shared/components/header/header.component';
 import { ReportCardComponent } from '@shared/components/report-card/report-card.component';
 import { StatCardComponent } from '@shared/components/stat-card/stat-card.component';
 
@@ -26,6 +29,7 @@ import { StatCardComponent } from '@shared/components/stat-card/stat-card.compon
     OrderCardComponent,
     ReportCardComponent,
     StatCardComponent,
+    HeaderComponent,
   ],
   imports: [
     CommonModule,
@@ -36,6 +40,8 @@ import { StatCardComponent } from '@shared/components/stat-card/stat-card.compon
     MatInputModule,
     MatTableModule,
     ReactiveFormsModule,
+    OverlayModule,
+    ToastrModule.forRoot(),
   ],
   exports: [
     ButtonComponent,
@@ -46,6 +52,7 @@ import { StatCardComponent } from '@shared/components/stat-card/stat-card.compon
     OrderCardComponent,
     ReportCardComponent,
     StatCardComponent,
+    HeaderComponent,
   ],
 })
 export class SharedModule {}
