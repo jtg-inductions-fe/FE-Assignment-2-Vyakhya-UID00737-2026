af91d5ec6783406f325818e34d2b1a0fc12a6da4
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-07-20 14:13:05 +0530

    VN_A2_06: restaurant list added

--- src/app/modules/restaurant/list-restaurant/list-restaurant.component.ts
+++ src/app/modules/restaurant/list-restaurant/list-restaurant.component.ts
@@ -27,7 +27,7 @@ export class ListRestaurantComponent implements OnInit {
     this.router.navigate([addRestaurant]);
   }
 
-  onEditClick(id: string): void {
+  onEditClick(id: number): void {
     this.router.navigate([editRestaurant, id]);
   }
 }
